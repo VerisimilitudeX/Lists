d3a3cfa4dae6aaf309c79eb04e43b907d6121d0a
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-15 12:28:54 -0700

    Update Birthday List.py

--- Birthday List.py
+++ Birthday List.py
@@ -1,5 +1,9 @@
 import random
+
 presents = ["computer", "airplane", "yacht", "phone"]
 print(presents)
+
 index = random.randint(0, len(presents) - 1)
-print(presents[index])
+random_item = presents[index]
+
+print(random_item)
